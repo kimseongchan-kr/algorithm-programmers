e19e99cebe9759ac1648b40d89688dd90dbf15f0
blockodyssey-sckim <fpel8800@gmail.com>
2022-09-22 00:24:14 +0900

    mod: 두 정수 사이의 합
    첫 번째 방법
    - 입력 받은 a, b를 smallNumber, bigNumber에 각각 할당합니다.
    - 만약 smallNumber와 bigNumber가 같다면 smallNumber를 반환합니다.
    - 만약 smallNumber가 bigNumber보다 크다면 smallNumber에 b를 bigNumber에 a를 할당합니다.
    - 위 작업이 끝나면 a부터 b까지 반복문을 돌리면 숫자를 더하고 계산된 값을 반환합니다.
    
    → 50.31ms이나 걸리는 케이스가 있어 수정 진행하겠습니다.
    
    두 번째 방법
    - for문으로 반복하면서 계속 더해가는 것이 아니라 계산으로 풀어보기로 했습니다.
    - samllNumber와 bigNumber를 구하는 전처리 작업은 첫 번째와 같습니다.
    - bigNumber에서 smallNumber를 뺀 값에 1을 더해 smallNumber에서 bigNumber까지의 수를 구해 inputLength에 담습니다.
    - smallNumber와 bigNumber을 더해 smallNumberPlusBigNumber에 담습니다.
    - middleNumber에 smallNumberPlusBigNumber가 만약 홀수라면 2로 나눠 담고 짝수라면 0을 담습니다.
    - 위 작업을 마치면 smallNumberPlusBigNumber * Math.floor(inputLength / 2) + middleNumber 작업을 진행해 값을 출력합니다.
    
    세 번째 방법
    - inputLength를 구할 때 Math.floor 작업을 진행해봤습니다.
    
    네 번째 방법
    - 두 번째 방법과 똑같지만 console.log() 빼니 속도가 더 줄었습니다.
    - 세 번째 방법과는 큰 차이가 없지만 아주 조금 빨랐습니다.

--- javascript/solution/level1/problem13.js
+++ javascript/solution/level1/problem13.js
@@ -2,8 +2,70 @@
  * 두 정수 사이의 합
  */
 
+// function solution(a, b) {
+//   var answer = 0;
+//   var smallNumber = a;
+//   var bigNumber = b;
+
+//   if (smallNumber === bigNumber) {
+//     return smallNumber;
+//   }
+
+//   if (smallNumber > bigNumber) {
+//     smallNumber = b;
+//     bigNumber = a;
+//   }
+
+//   for (let i = smallNumber; i <= bigNumber; i++) {
+//     answer = answer + i;
+//   }
+
+//   return answer;
+// }
+
+// function solution(a, b) {
+//   var smallNumber = a;
+//   var bigNumber = b;
+
+//   if (smallNumber === bigNumber) {
+//     return smallNumber;
+//   }
+
+//   if (smallNumber > bigNumber) {
+//     smallNumber = b;
+//     bigNumber = a;
+//   }
+
+//   var inputLength = bigNumber - smallNumber + 1;
+//   var smallNumberPlusBigNumber = smallNumber + bigNumber;
+//   var middleNumber = smallNumberPlusBigNumber % 2 === 0 ? smallNumberPlusBigNumber / 2 : 0;
+
+//   console.log(inputLength, smallNumberPlusBigNumber, middleNumber);
+
+//   return smallNumberPlusBigNumber * Math.floor(inputLength / 2) + middleNumber;
+// }
+
+// function solution(a, b) {
+//   var smallNumber = a;
+//   var bigNumber = b;
+
+//   if (smallNumber === bigNumber) {
+//     return smallNumber;
+//   }
+
+//   if (smallNumber > bigNumber) {
+//     smallNumber = b;
+//     bigNumber = a;
+//   }
+
+//   var halfAllNumber = Math.floor((bigNumber - smallNumber + 1) / 2);
+//   var smallNumberPlusBigNumber = smallNumber + bigNumber;
+//   var middleNumber = smallNumberPlusBigNumber % 2 === 0 ? smallNumberPlusBigNumber / 2 : 0;
+
+//   return smallNumberPlusBigNumber * halfAllNumber + middleNumber;
+// }
+
 function solution(a, b) {
-  var answer = 0;
   var smallNumber = a;
   var bigNumber = b;
 
@@ -16,11 +78,11 @@ function solution(a, b) {
     bigNumber = a;
   }
 
-  for (let i = smallNumber; i <= bigNumber; i++) {
-    answer = answer + i;
-  }
+  var halfAllNumber = Math.floor((bigNumber - smallNumber + 1) / 2);
+  var smallNumberPlusBigNumber = smallNumber + bigNumber;
+  var middleNumber = smallNumberPlusBigNumber % 2 === 0 ? smallNumberPlusBigNumber / 2 : 0;
 
-  return answer;
+  return smallNumberPlusBigNumber * halfAllNumber + middleNumber;
 }
 
-console.log(solution(3, 3));
+console.log(solution(5, 3));
