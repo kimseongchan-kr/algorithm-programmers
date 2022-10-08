dc4205ed7ca7416876dfd0bc3bf9419b0f811ae2
kimseongchan-kr <fpel8800@gmail.com>
2022-10-08 23:29:37 +0900

    add: 행렬의 덧셈 - 입력 받은 배열의 크기 만큼 배열을 돌립니다. - 입력 받은 배열이 이차원배열이므로 반복문 안쪽에서 반복문을 돌려가며 해당 배열의 숫자를 가져옵니다. - 가져온 숫자를 더하고 가져온 index 배열에 담아줍니다. - 반복문이 끝나고 answer를 리턴합니다.

---
+++ javascript/solution/level1/problem27.js
@@ -0,0 +1,26 @@
+function solution(arr1, arr2) {
+  let answer = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    answer.push([]);
+
+    for (let j = 0; j < arr1[i].length; j++) {
+      answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [1, 2],
+      [2, 3],
+    ],
+    [
+      [3, 4],
+      [5, 6],
+    ]
+  )
+);
